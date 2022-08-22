56f09de09db7ba83a5db7c74dd3c4c86972454da
Rodrigo-Valle <rodrigocostavalle@gmail.com>
2022-08-22 13:25:19 -0300

    criada entidade despesas

---
+++ src/entity/expenses.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryColumn, ManyToOne } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+import { User } from "./index"
+
+@Entity()
+export class Expense {
+    @PrimaryColumn()
+    public id: string;
+
+    @Column({ name: 'descricao' })
+    public description: string;
+
+    @Column({ name: "valor" })
+    public value: number;
+
+    @Column({ name: "data" })
+    public date: Date;
+
+    @ManyToOne(() => User, (user: User) => user.id, { onDelete: "CASCADE" })
+    user: User
+
+    constructor(description: string, value: number, date: Date, user: User, id?: string) {
+        this.description = description;
+        this.value = value;
+        this.date = date;
+        this.user = user;
+        this.id = id || uuidv4();
+    }
+}
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryColumn, OneToMany } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
+import { Expense } from "./expenses.entity";
 import { Income } from "./income.entity";
 
 @Entity()
@@ -21,6 +22,9 @@ export class User {
 
     @OneToMany(() => Income, (income) => income.user)
     public income?: Income[]
+
+    @OneToMany(() => Expense, (expense) => expense.user)
+    public expense?: Expense[]
 
     constructor(cpf: string, name: string, email: string, password: string, id?: string) {
         this.cpf = cpf;
